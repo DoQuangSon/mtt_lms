669968665d5665c34c4623b9474d80ca2db7da77
hieudb <hieudb@fabbi.com.vn>
2023-09-05 15:01:01 +0700

    tÃest

--- src/main/java/com/fabbi/mtt_lms/config/SecurityConfiguration.java
+++ src/main/java/com/fabbi/mtt_lms/config/SecurityConfiguration.java
@@ -18,8 +18,8 @@ import org.springframework.security.web.authentication.logout.LogoutHandler;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
@@ -29,54 +29,14 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
-                .authorizeRequests()
-                .requestMatchers()
-                .permitAll()
+                .authorizeRequests(
+                        auth -> {
+                            auth.antMatchers("/", "/api/v1/auth/**").permitAll();
+                            auth.antMatchers("/user").hasRole("USER");
+                            auth.antMatchers("/admin").hasRole("ADMIN");
+                        });
 
-                // TODO
-                //
-                //
-                //                .requestMatchers("/api/v1/management/**").hasAnyRole(ADMIN.name(),
-                // MANAGER.name())
-                //
-                //
-                //                .requestMatchers(GET,
-                // "/api/v1/management/**").hasAnyAuthority(ADMIN_READ.name(), MANAGER_READ.name())
-                //                .requestMatchers(POST,
-                // "/api/v1/management/**").hasAnyAuthority(ADMIN_CREATE.name(),
-                // MANAGER_CREATE.name())
-                //                .requestMatchers(PUT,
-                // "/api/v1/management/**").hasAnyAuthority(ADMIN_UPDATE.name(),
-                // MANAGER_UPDATE.name())
-                //                .requestMatchers(DELETE,
-                // "/api/v1/management/**").hasAnyAuthority(ADMIN_DELETE.name(),
-                // MANAGER_DELETE.name())
-
-                //        request -> {
-                //            "/api/v1/auth/**",
-                //                    "/v2/api-docs",
-                //                    "/v3/api-docs",
-                //                    "/v3/api-docs/**",
-                //                    "/swagger-resources",
-                //                    "/swagger-resources/**",
-                //                    "/configuration/ui",
-                //                    "/configuration/security",
-                //                    "/swagger-ui/**",
-                //                    "/webjars/**",
-                //                    "/swagger-ui.html"
-                //        }
-
-                /* .requestMatchers("/api/v1/admin/**").hasRole(ADMIN.name())
-
-                .requestMatchers(GET, "/api/v1/admin/**").hasAuthority(ADMIN_READ.name())
-                .requestMatchers(POST, "/api/v1/admin/**").hasAuthority(ADMIN_CREATE.name())
-                .requestMatchers(PUT, "/api/v1/admin/**").hasAuthority(ADMIN_UPDATE.name())
-                .requestMatchers(DELETE, "/api/v1/admin/**").hasAuthority(ADMIN_DELETE.name())*/
-
-                .anyRequest()
-                .authenticated()
-                .and()
-                .sessionManagement()
+        http.sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authenticationProvider(authenticationProvider)
